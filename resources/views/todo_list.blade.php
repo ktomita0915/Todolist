e1144ecb479a1f0d42c87d36d19523cc67ba0bb8
kento <kento@kentonoMacBook-Air.local>
2021-07-27 18:49:42 +0900

    first-commit

--- resources/views/todo_list.blade.php
+++ resources/views/todo_list.blade.php
@@ -6,50 +6,51 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Todolist</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 
 <body>
-  <h1>Todo List</h1>
-  <div>
-    <form acction="/" method="POST">
-      @csrf
-      @if ($errors->any())
-      <ul>
-      @foreach ($error->all() as $error)
-      <li style="color:red">{{$error}}</li>
-      @endforeach
-      </ul>
-      @endif
-      <input type="text",name="body"class="form-control" placeholder="todolist" style="/">
-      <button type="submit"class="btn btn-primary">追加する</button>
-      </form>
-      <tr>
-        <th>作成日</th>
-        <th>タスク名</th>
-        <th>更新</th>
-        <th>削除</th>
-      </tr>
-      <tbody>
-      @foreach($todos as $todo)
-      <tr>
-        <td>{{$todos->task_date}}</td>
-        <td>{{$todo->task_name}}</td>
-        <td><form action="{{action('TodolistFormController',$todo)}}" method="post">
-        {{csrf_field()}}
-        {{method_field('get')}}
-        <button type="submit" class="btn btn-primary">更新</button>>
-       </td>
-        </form>
-        <td><form action="{{url('/todos',$todo->id)}}"method="post">
-        {{csrf_field()}}
-        {{method_field('delete')}}
-        <button type="submit"class="btn btn-danger">削除</button>
-        </td>
-        </form>
-      </tr>
-      @endforeach
-    </table>
+  <div class="container mt-3">
+    <h1>Todo List</h1>
   </div>
-</body>
-
+  <div class="container mt-3">
+    <div class="container mb-4">
+      {!!Form::open(['route'=>'todos.store','method'=>post])!!}
+      {{csrf_field()}}
+      <div class="row">
+        {{Form::text('newTodo',null,['class'=>'form-control col-8 mr-5'])}
+        {{Form::submit('追加',['class'=>'btn btn-primary'])}}
+      </div>
+      {!!Form::close()!!}
+      </div>
+      @if($error->has('newTodo'))
+      <p class="alert alert-danger">{{$error->first('newTodo')}}</p>
+      @endif
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">作成日</th>
+            <th scope="col" style="width=60%">タスク名</th>
+            <th scope="col">更新</th>
+            <th scope="col">削除</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>2021-07-26 06:42:23</td>
+            <th scope="row">タスク名</th>
+            <td><a href="" class-"btn btn primary">更新</a></td>
+            <td>{{Form::submit('削除',['class'=>'btn btn-danger'])}}</td>
+          </tr>
+          <tr>
+            <td>2021-07-26 06:42:23</td>
+            <th scope="col" style="width=60%">タスク名</th>
+            <td><a href=""class="btn btn primary">更新</th>
+            <td>{{Form::submit('削除',['class'=>'btn btn danger'])}}</td>
+          </tr>
+          @endforeach
+        </tbody>
+        </table>
+        </div>
+        </body>
 </html>
